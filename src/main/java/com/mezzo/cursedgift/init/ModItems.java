1750d4d839049a0b740e82363173df2e1cce9996
Wobbe <55002389+Mystellic@users.noreply.github.com>
2019-09-11 22:56:41 +0200

    ADDED magix sword

--- src/main/java/com/mezzo/cursedgift/init/ModItems.java
+++ src/main/java/com/mezzo/cursedgift/init/ModItems.java
@@ -4,13 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.mezzo.cursedgift.items.ItemBase;
+import com.mezzo.cursedgift.items.tools.ToolSword;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemSword;
+import net.minecraftforge.common.util.EnumHelper;
 
 public class ModItems 
 {
 
 	public static final List<Item> ITEMS =new ArrayList<Item>();
+	
+	//ToolMaterials tab, adding new materials for tools
+	public static final ToolMaterial MATERIAL_MAGIX = EnumHelper.addToolMaterial("material_magix",3,2500, 10.0F, 3.5F, 10);
+	//material, harvestlevel, durability, speed, damage, enchantability(how much you get good enchants)
+	
+	
 	
 	
 	//add modded items in game
@@ -22,5 +32,8 @@ public class ModItems
 	public static final Item AVERAGEMATTER = new ItemBase("unstable_average_matter");
 	public static final Item MAXIGINGOT = new ItemBase("pure_magic_ingot");
 	
+	
+	//Tools
+	public static final ItemSword MAGIX_SWORD = new ToolSword("magix_sword",MATERIAL_MAGIX);
 	
 }
---
+++ src/main/java/com/mezzo/cursedgift/items/tools/ToolSword.java
@@ -0,0 +1,28 @@
+package com.mezzo.cursedgift.items.tools;
+
+import com.mezzo.cursedgift.Main;
+import com.mezzo.cursedgift.init.ModItems;
+import com.mezzo.cursedgift.util.IHasModel;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemSword;
+
+public class ToolSword extends ItemSword implements IHasModel
+{
+	public ToolSword(String name, ToolMaterial material) 
+	{
+		super(material);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.MATERIALS);
+		
+		ModItems.ITEMS.add(this);
+		
+	}
+	
+	@Override
+	public void registerModels() 
+	{ //render it in inventory
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+	}
+}
